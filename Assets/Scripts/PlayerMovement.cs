4d442f189c611eb29db3b8c273daf074cb086b7d
SandroCJ210 <sandro.carrillo.j@uni,pe>
2024-11-14 17:10:09 -0500

    feat: add player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,38 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    void Move(){
-        Debug.Log("wosh");
+    [SerializeField] private float speed;
+    private Vector2 input;
+    private PlayerInput playerInput;
+    private Rigidbody rb;
+    private void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        GetInput();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector3(input.x, rb.linearVelocity.y, input.y) * speed;
+    }
+
+    public void Move(InputAction.CallbackContext context){
+        if (context.performed)
+        {
+            Debug.Log("wosh");
+        }
+    }
+
+    private void GetInput()
+    {
+        input = playerInput.actions["Move"].ReadValue<Vector2>();
     }
 }
